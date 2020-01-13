953f8153ef57739001708dff85eee0c5aaf660ea
moguranran <peehssalg@gmail.com>
2020-01-13 12:17:33 +0900

    特性の抜き出し方を確認

--- WebApp/WebApp/PokemonSpeedCheckerApp/views.py
+++ WebApp/WebApp/PokemonSpeedCheckerApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import pokebase as pb
 from django.views import generic
+import PokemonSpeedCheckerApp.models
 
 # Create your views here.
 
@@ -11,9 +12,16 @@ def speed_checker(request):
 class IndexView(generic.TemplateView):
     template_name = 'PokemonSpeedCheckerApp\pokedex.html'
 
+    def set_param(self, context, name):
+        pokemon = PokemonSpeedCheckerApp.models.Pokemon()
+        pokemon.speed = context[name].name
+        print(pokemon.speed)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         pikachu = pb.pokemon(25)
         print(pikachu)
         context['pikachu'] = pikachu
+        self.set_param(context, 'pikachu')
         return context
+
